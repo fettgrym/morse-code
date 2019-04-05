46abb048543ac722fe6f79fc630b6671235d5cab
fettgrym <15312902+fettgrym@users.noreply.github.com>
2019-04-05 19:50:12 -0400

    Add files via upload
    Fully working encoder and decoder.

--- morse_code.py
+++ morse_code.py
@@ -43,28 +43,87 @@ def encode(message):
 # Dictonary with all caracters/numbers to morse code
 
 	message = message.upper()
-	codedmsg = ""
+	encoded_msg = ""
 # Makes you massage in all CAPS, and creates an empy string
 
 	for x in message:
-		codedmsg = codedmsg + morse_code[x] + " "
+		encoded_msg = encoded_msg + morse_code[x] + " "
 # Takes your message imput and turns it into morse code
 
-	return codedmsg
+	return encoded_msg
 # Returns the morse code
 
 def decode(secret):
-# function that decodes your string
+# Function that decodes your string
 
-	pass
+	morse_code_decode = {
+	".-": "A",
+	"-...": "B",
+	"-.-.": "C",
+	"-..": "D",
+	".": "E",
+	"..-.": "F",
+	"--.": "G",
+	"....": "H",
+	"..": "I",
+	".---": "J",
+	"-.-": "K",
+	".-..": "L",
+	"--": "M",
+	"-.": "N",
+	"---": "O",
+	".--.": "P",
+	"--.-": "Q",
+	".-.": "R",
+	"...": "S",
+	"-": "T",
+	"..-": "U",
+	"...-": "V",
+	".--": "W",
+	"-..-": "X",
+	"-.--": "Y",
+	"--..": "Z",
+	"-----": "0",
+	".----": "1",
+	"..---": "2",
+	"...--": "3",
+	"....-": "4",
+	".....": "5",
+	"-....": "6",
+	"--...": "7",
+	"---..": "8",
+	"----.": "9",
+	"(space)": " "
+	}
+# Dictonary for decoding morse code
 
+	secret = secret.split(" ")
+# Splites the morse code at every space
 
+	decoded_msg = ""
+# Creates a empy string
+
+	try:
+		for x in secret:
+			decoded_msg = decoded_msg + morse_code_decode[x]
+	except KeyError:
+		pass
+# Takes the morse code from the dir and turns it into caracters or numbers
+
+	decoded_msg = decoded_msg.title()
+# Makes the decoded word display in title
+
+	return decoded_msg
+# Returns a decoded message
 
 if __name__ == "__main__":
-# execute program if started from original file
+# Execute test-program if started from original file
 
 	secret = encode("Hello World")
+	nosecret = decode(secret)
 # Defines secret as the string with the morse code
 
-	print(secret)
-# Prints the morse code
+	print("Hello World = " + secret + "\n")
+	print(secret + "= " + nosecret)
+	print('\nTo import this to your own python project do "from morse_code import *"')
+# Prints the morse code and instructions
